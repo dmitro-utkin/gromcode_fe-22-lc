f265a4ef59a7dd69f611a05a46f7816b712a77ab
dmitro-utkin <sssddd1366628@gmail.com>
2024-06-11 11:57:24 +0300

    function lesson

---
+++ 11-06-lc-3-functions/index.js
@@ -0,0 +1,144 @@
+// // console.log('Hello FE-22-lc-3');
+
+// /*
+//  * ф-ция getSenseOfLife не должна принимать аргументы
+//  * ф-ция getSenseOfLife должна вернуть число 42
+//  */
+// // const getSenseOfLife = () => {
+// //     return 42;
+// // };
+
+// // function getSenceOfLife() {
+// //     // initial code
+// //     console.log(42);
+// // }
+
+// // call getSenseOfLife function
+// // getSenceOfLife();
+
+// // calling the function with the return value
+// // const res = getSenceOfLife();
+// // console.log(res);
+
+// // console.log(getSenceOfLife());
+
+// // ----------------------------------
+// // ----------------------------------
+// // ----------------------------------
+
+// /* ф-ция getSquared должна принимать число и вернуть квадрат этого числа */
+
+// // const getSquared = num => {
+// //   // return num ** 2;
+// //   return num * num;
+// // };
+
+// // // testing
+// // console.log(getSquared(5));
+// // console.log(getSquared(-4));
+// // console.log(getSquared(0));
+
+// // ----------------------------------
+// // ----------------------------------
+// // ----------------------------------
+
+// /*
+//  * ф-ция printMsg должна принимать число
+//  * ф-ция printMsg должна вывести в консоль строку 'I am 34 years old'
+//  * где 34 - число, полученное в аргументах
+//  */
+
+// // const printMsg = age => {
+// //   // return `I am ${age} years old`
+// //   console.log(`I am ${age} years old`);
+// // };
+
+// // console.log(printMsg(34));
+// // console.log(printMsg(18));
+// // console.log(printMsg(66));
+
+// // _________________________________________
+
+// // function getMsg(age) {
+// //   if (age < 18) {
+// //     return null;
+// //   }
+// //   return `I am ${age} years old`;
+// // }
+
+// // console.log(getMsg(34));
+// // console.log(getMsg(42));
+// // console.log(getMsg('28'));
+// // console.log(getMsg('17'));
+
+// // -----------------------------------------
+// // -----------------------------------------
+// // -----------------------------------------
+
+// // Функція потрібна для структуризації коду!!!!!!!!!!!
+
+// // -----------------------------------------
+// // -----------------------------------------
+// // -----------------------------------------
+// // ARROW FUNCTIONS
+// // -----------------------------------------
+// // -----------------------------------------
+// // -----------------------------------------
+
+// /* ф-ция mult должна принимать два числа и вернуть их произведение */
+// const mult = (num1 = 0, num2 = 0) => {
+//     console.log(num1, num2);
+//     return num1 * num2
+// };
+// const sum = (num1, num2) => num1 + num2;
+// const diff = (num1, num2) => num1 - num2;
+
+// console.log(mult(5, 6));
+// console.log(mult(10, -10));
+// console.log(mult('-10', -10));
+// console.log(mult(null, true));
+// // console.log(mult());
+
+
+// // -----------------------------------------
+// // -----------------------------------------
+// // -----------------------------------------
+
+// /* ф-ция square должна принимать число и вернуть его, возведенное в квадрат */
+
+// // const squared = (num) => {
+// //     return num * num
+// // };
+
+// // const squared = num => num * num;
+
+// // // testing
+// // console.log(getSquared(5));
+// // console.log(getSquared(-4));
+// // console.log(getSquared(0));
+
+// // -----------------------------------------
+// // -----------------------------------------
+// // -----------------------------------------
+
+// // const getMagicNumber = () => 17;
+
+// // console.log(getMagicNumber());
+// // console.log(getMagicNumber('Hello!'));
+
+// // -----------------------------------------
+// // -----------------------------------------
+// // -----------------------------------------
+// // get sum + diff + mult
+// // const mathFunc = (a, b) => {
+// //   return sum(a, b) + mult(a, b) + diff(a, b);
+// // };
+// // // 5 + 6 - 1 = 10
+
+// // console.log(mathFunc(2, 3));
+
+// // -----------------------------------------
+// // -----------------------------------------
+// // -----------------------------------------
+
+// // const sum = (num1 = 0, num2 = 0) => num1 + num2;
